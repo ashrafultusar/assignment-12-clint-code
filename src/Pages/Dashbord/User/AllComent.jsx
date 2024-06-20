392f1d98461df01cf53fd39d59b85ab7158d95d5
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-06-20 18:11:41 +0600

    added all update

--- src/Pages/Dashbord/User/AllComent.jsx
+++ src/Pages/Dashbord/User/AllComent.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { axiosCommon } from '../../../Hook/useAxiosCommon';
 import { useParams } from 'react-router-dom';
 import AllCommentSingleTable from './AllCommentSingleTable';
+import Sidebar from '../../../Component/Dashbord/Sidebar';
 
 const AllComent = () => {
     
@@ -24,10 +25,19 @@ const AllComent = () => {
 console.log(comments);
 
     return (
-        <div>
-        {
+      <div >
+        <Sidebar></Sidebar>
+        {/* {
           comments.map(comnt=><AllCommentSingleTable comnt={comnt} key={comnt._id}></AllCommentSingleTable>)
-            }
+            } */}
+         {comments && comments.length > 0 ? (
+        comments.map(comnt => (
+          <AllCommentSingleTable comnt={comnt} key={comnt._id} />
+        ))
+      ) : (
+        <p className='container mx-auto mt-96 text-center text-2xl font-bold text-red-400'>No comment On this post</p>
+      )}
+
         </div>
     );
 };
